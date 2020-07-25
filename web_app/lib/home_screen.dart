bdf9bb04a8db7b918e55d9c46cffc7b861b4dbeb
Santiago Gonzalez Fernandez <santiagogonzalezfernandezpro@gmail.com>
2020-07-25 13:20:45 -0300

    Avanzo en la web con flutter

---
+++ web_app/lib/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:web_app/sections/topSection/top_section.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            TopSection()
+          ],
+        ),
+      ),
+    );
+  }
+}
